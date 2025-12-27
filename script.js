c0faa56ecf06c35945fc1b1208a6eda06c1f7893
ankit nishad <ankitnishad3333@gmail.com>
2025-12-27 18:42:50 +0530

    jhj

---
+++ script.js
@@ -0,0 +1,22 @@
+let timeLeft = 20;
+const timerEl = document.getElementById("timer");
+const circle = document.querySelector(".progress-ring__circle");
+const circumference = 2 * Math.PI * 60;
+
+circle.style.strokeDasharray = circumference;
+
+function setProgress(time) {
+    const offset = circumference - (time / 20) * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+const countdown = setInterval(() => {
+    timeLeft--;
+    timerEl.innerText = timeLeft;
+    setProgress(timeLeft);
+
+    if (timeLeft <= 0) {
+        clearInterval(countdown);
+        window.location.href = "https://t.me/+uI6alAqnruxjMjc1";
+    }
+}, 1000);
